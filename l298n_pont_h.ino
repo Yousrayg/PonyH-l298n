af64cd00f05a3432d05e7f1fecc8693acb10cfbe
Yousrayg <45716415+Yousrayg@users.noreply.github.com>
2018-12-08 17:28:23 +0100

    Add files via upload

--- l298n_pont_h.ino
+++ l298n_pont_h.ino
@@ -1,24 +1,41 @@
-int ENA =5;
+int ENA = 5;
+int ENB = 4;
 int in1 = 6;
-int in2 =7;
+int in2 = 7;
+int in3 = 8;
+int in4 = 9;
 
 void setup() {
-  
-pinMode(ENA,OUTPUT)
-pinMode(in1,OUTPUT)
-pinMode(in2,OUTPUT)
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 }
 
 void loop() {
- digitalWrite(ENA,HIGH);
- digitalWrite(in1;HIGH);
- digitalWrite(in2,LOW);
- delay(1000);
- digitalWrite(ENA,LOW);
- delay(1000);
- analogWrite(ENA,120);
- delay(1000);
- digitalWrite(ENA,LOW);
- delay(1000)
+  digitalWrite(ENA, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  delay(1000);
+  digitalWrite(ENA, HIGH);
+  delay(1000);
+  analogWrite(ENA, 220);
+  delay(1000);
+  digitalWrite(ENA, HIGH);
+  delay(1000);
+
+  digitalWrite(ENB, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(1000);
+  digitalWrite(ENB, HIGH);
+  delay(1000);
+  analogWrite(ENB, 220);
+  delay(1000);
+  digitalWrite(ENB, HIGH);
+  delay(1000);
 
 }
